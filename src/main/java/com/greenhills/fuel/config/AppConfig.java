0a763ba84139b09e84025438425720f24b252645
getbehindthmule <gerardsavage@me.com>
2019-01-31 11:28:39 +0000

    add simple Swagger UI support

--- src/main/java/com/greenhills/fuel/config/AppConfig.java
+++ src/main/java/com/greenhills/fuel/config/AppConfig.java
@@ -1,11 +1,25 @@
 package com.greenhills.fuel.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @PropertySource("classpath:application-config.yml")
+@EnableSwagger2
 public class AppConfig {
-    // useful later if application becomes too complex for autowiring
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.greenhills.fuel"))
+                .paths(PathSelectors.any())
+                .build();
+    }
 
 }
